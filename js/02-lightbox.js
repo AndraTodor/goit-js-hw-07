96697360bc05f3fdd7c61dfe12e275031b122835
AndraTodor <andra_t27@yahoo.com>
2024-06-19 22:23:41 +0300

    tema 2 - lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            title="${description}"
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "title", // Folosește atributul alt pentru caption
+  captionDelay: 250, // Întârziere de 250ms pentru afișarea caption-ului
+});
 
 console.log(galleryItems);
